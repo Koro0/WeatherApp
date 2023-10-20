ae03ce2a7cda4875265a33a7a9e1b246b5552471
Koro0 <ledane92@gmail.com>
2023-10-20 11:12:45 +0200

    change to use ip for location

--- src/components/getLocatistion.jsx
+++ src/components/getLocatistion.jsx
@@ -1,33 +1,26 @@
-import React, { useContext, useEffect, } from 'react';
+import React, { useContext, useEffect, useState, } from 'react';
 import Context from '../context/geolocation.context';
 
 export default function GetLocatistion() {
-  const { setLat } = useContext(Context);
-  const { setLng } = useContext(Context);
-
+  const { ipData, setIpData } = useContext(Context);
   useEffect(() => {
-    if ('geolocation' in navigator) {
-      // La géolocalisation est disponible
-      navigator.geolocation.getCurrentPosition(
-        function (position) {
-          // Callback en cas de succès
-          const latitude = position.coords.latitude;
-          const longitude = position.coords.longitude;
-          console.log(`Latitude : ${latitude}, Longitude : ${longitude}`);
-          setLat(latitude);
-          setLng(longitude);
-        },
-        function (error) {
-          // Callback en cas d'erreur
-          console.error('Erreur de géolocalisation : ' + error.message);
+    const fetchWithIP = async () => {
+      try {
+        const response = await fetch("https://api.ipify.org/?format=json");
+        if (response.ok) {
+          const data = response.json();
+          setIpData(data);
+        } else {
+          console.error("La requête a échoué avec le statut : " + response.status);
         }
-      );
-    } else {
-      // La géolocalisation n'est pas prise en charge
-      console.log(
-        "La géolocalisation n'est pas disponible dans ce navigateur."
-      );
+      } catch (err) {
+        console.error(err)
+      }
+
     }
+    fetchWithIP();
+    console.log(fetchWithIP());
+
   }, []);
 
   return <>
