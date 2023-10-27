6844db4289021c192d7e89ed64ecd95a32adefbe
Koro0 <ledane92@gmail.com>
2023-10-27 16:16:33 +0200

    delete contexte

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Context from './context/geolocation.context';
 import IndexPage from './pages/index';
 import Error from './components/error';
 
@@ -8,13 +7,11 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Context.Provider>
-          <header className="App-header"></header>
-          <Routes>
-            <Route path="/" element={<IndexPage />} />
-            <Route path="/*" element={<Error />} />
-          </Routes>
-        </Context.Provider>
+        <header className="App-header"></header>
+        <Routes>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/*" element={<Error />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
